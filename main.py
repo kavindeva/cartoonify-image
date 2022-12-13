5a8e42a2c768930ecc208b96b09e1c956e4474b3
Kavin <kavindmailme@gmail.com>
2022-12-13 15:18:54 +0530

    Initiated the project.

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import numpy as np
+
+
+# Read in an image
+img = cv.imread('../Resources/Photos/park.jpg')
+cv.imshow('Park', img)
+
+output = np.array(image)
+x, y, c = output.shape
+
+# takes input image as parameter
+def cartoon(image):
+    # returns the output image
+    return output
